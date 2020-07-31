8c9fbdbcfadcc5a8c4c7eca5d47646589ab779ab
Tayrone <jose.tayrone@ctis.com.br>
2020-07-31 07:13:40 -0300

    CRUD - Products

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const requireDir = require('require-dir');
 
 // INICIANDO O APP
 const app = express();
+// ENVIAR PARA APLICAÇÃO NO FORMATO JSON
+app.use(express.json());
 
 // INICIANDO O DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', 
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const routes = express.Router();
 
 const ProductController = require('../src/controllers/ProductController')
 
-routes.get('/products', ProductController.index);
+routes.get('/products', ProductController.list);
+routes.get('/products/:id', ProductController.show)
+routes.post('/products', ProductController.create);
+routes.delete('/products/:id', ProductController.destroy);
+routes.put('/products/:id', ProductController.update);
 
 module.exports = routes;
